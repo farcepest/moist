c27d056b97ec92d22197526922a9b587a99d7b04
kylev <none@none>
2009-02-15 07:26:43 +0000

    Update some of the constants and declarations to post python-2.0 convenience functions, increase related test coverage.
    --HG--
    branch : MySQLdb
    extra : convert_revision : svn%3A9fc6cd9a-920d-0410-adcf-ac96716ed7e8/trunk/MySQLdb%40586

--- tests/test_MySQLdb_nonstandard.py
+++ tests/test_MySQLdb_nonstandard.py
@@ -1,5 +1,6 @@
 import unittest
 
+import _mysql
 import MySQLdb
 from MySQLdb.constants import FIELD_TYPE
 
@@ -18,46 +19,57 @@ class TestDBAPISet(unittest.TestCase):
         self.assertTrue(FIELD_TYPE.DATE != MySQLdb.STRING)
 
 
-class NonStandard(unittest.TestCase):
-    """Test _mysql.connection internals."""
+class CoreAPI(unittest.TestCase):
+    """Test _mysql internals."""
 
     def setUp(self):
-        self.conn = MySQLdb.connect(db='test')
+        self.conn = _mysql.connect(db='test')
 
     def tearDown(self):
         self.conn.close()
 
+    def test_NULL(self):
+        """Should have a NULL constant."""
+        self.assertEqual(_mysql.NULL, 'NULL')
+
+    def test_version(self):
+        """Version information sanity."""
+        self.assertTrue(isinstance(_mysql.__version__, str))
+
+        self.assertTrue(isinstance(_mysql.version_info, tuple))
+        self.assertEqual(len(_mysql.version_info), 5)
+
     def test_thread_id(self):
-        tid = self.conn._db.thread_id()
+        tid = self.conn.thread_id()
         self.assertTrue(isinstance(tid, int),
                         "thread_id didn't return an int.")
 
-        self.assertRaises(TypeError, self.conn._db.thread_id, ('evil',),
+        self.assertRaises(TypeError, self.conn.thread_id, ('evil',),
                           "thread_id shouldn't accept arguments.")
 
     def test_affected_rows(self):
-        self.assertEquals(self.conn._db.affected_rows(), 0,
+        self.assertEquals(self.conn.affected_rows(), 0,
                           "Should return 0 before we do anything.")
 
 
     def test_debug(self):
         # FIXME Only actually tests if you lack SUPER
         self.assertRaises(MySQLdb.OperationalError,
-                          self.conn._db.dump_debug_info)
+                          self.conn.dump_debug_info)
 
     def test_charset_name(self):
-        self.assertTrue(isinstance(self.conn._db.character_set_name(), str),
+        self.assertTrue(isinstance(self.conn.character_set_name(), str),
                         "Should return a string.")
 
     def test_host_info(self):
-        self.assertTrue(isinstance(self.conn._db.get_host_info(), str),
+        self.assertTrue(isinstance(self.conn.get_host_info(), str),
                         "Should return a string.")
 
     def test_proto_info(self):
-        self.assertTrue(isinstance(self.conn._db.get_proto_info(), int),
+        self.assertTrue(isinstance(self.conn.get_proto_info(), int),
                         "Should return an int.")
 
     def test_server_info(self):
-        self.assertTrue(isinstance(self.conn._db.get_server_info(), str),
+        self.assertTrue(isinstance(self.conn.get_server_info(), str),
                         "Should return an str.")
 
