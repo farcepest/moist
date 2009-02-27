b0072ff56395b33f368e35805dd57956d8d2babc
kylev <none@none>
2009-02-27 19:46:55 +0000

    python 2.4 is our minimum version now
    --HG--
    branch : MySQLdb
    extra : convert_revision : svn%3A9fc6cd9a-920d-0410-adcf-ac96716ed7e8/trunk/MySQLdb%40605

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import os
 import sys
 from setuptools import setup, Extension
 
-if not hasattr(sys, "hexversion") or sys.hexversion < 0x02030000:
-    raise Error("Python 2.3 or newer is required")
+if not hasattr(sys, "hexversion") or sys.hexversion < 0x02040000:
+    raise Error("Python 2.4 or newer is required")
 
 if os.name == "posix":
     from setup_posix import get_config
